0ddca770382ad8c4ff049fedd0fc737d12408d8f
MecryTv <mail.MecryTv@gmail.com>
2026-05-14 23:59:33 +0200

    [ADD] Database CleanUp Runnable

---
+++ src/runnables/DatabaseCleanup.js
@@ -0,0 +1,27 @@
+const logger = require('../utils/logger');
+const Guardian = require('../services/Guardian');
+
+module.exports = {
+    name: "DatabaseCleanup",
+    type: "runnable",
+    daily: true,
+    time: "03:00",
+
+    async execute(client) {
+        if (!client.database) {
+            return logger.error("[Task] Database service not initialized. Skipping Database Cleanup.");
+        }
+
+        try {
+            logger.info("[Task] Starting Database Cleanup...");
+
+            await client.database.cleanupSoftDeletes();
+
+            logger.info("[Task] Database Cleanup successfully completed.");
+        } catch (error) {
+            logger.error(`[Task] Error while CleanUp: ${error.message}`);
+
+            await Guardian.handleGeneric("Daily Cleanup Task Failed", error);
+        }
+    }
+};
